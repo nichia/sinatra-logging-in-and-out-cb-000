de6652a862d14c32c28ab55b5f3de689094a3d3c
nichia <nichia@gmail.com>
2018-05-18 18:11:08 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,14 @@
 class Helpers
+  def self.current_user(session)
+    User.find_by_id(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    # check if session[:user_id] == nil, user is not logged in
+    if session[:user_id] == nil
+      false
+    else
+      true
+    end
+  end
 end
